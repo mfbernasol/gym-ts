74d4982ad4ba49e5ed3267e5f97ce77fb274ad4d
mfbernasol <mfbernasol@gmail.com>
2023-04-27 09:55:34 -0500

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from '@/scenes/navbar'
+import { useState } from 'react';
+import { SelectedPage } from '@/shared/types';
+
 function App() {
-  return <div className='app'>app</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("home")
+
+  return <div className='app bg-gray-20'>
+    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+  </div>;
 }
 
 export default App;
